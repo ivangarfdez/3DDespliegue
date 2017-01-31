8b8054dfcde74faef69d07cc96f84a48caf6ab64
ivangarfdez <ivangarfdez@gmail.com>
2017-01-31 18:24:07 +0100

    Ejercicio D

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+
 apt-get -y install apache2
 sudo rm -rf /var/www/
 ln -s /home/proyectosJuanmi/ /var/www
@@ -11,3 +13,12 @@ sudo a2ensite ejercicio1.com.conf
 sudo a2ensite ejercicio2.com.conf
 
 service apache2 reload
+
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt
+sudo mysql_install_db
+
+sudo rm /etc/mysql/my.cnf
+sudo cp /home/configuracion/my.cnf /etc/mysql
+sudo service mysql restart
